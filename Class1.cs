42e27c53f06f20383518adb059081ea55f4f2816
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-10-29 12:17:23 +0530

    October 29 2024
    notes on call by value/call by ref
    indexers
    enum
    out
    obsolete

---
+++ Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2
+{
+    internal class Class1
+    {
+
+
+        public class Area
+        {
+
+            //public int Calc(int l, int w)
+            //{
+            //    l = l + 2;
+            //    w = w + 2;
+            //    int area = l * w;
+            //    Console.WriteLine($"vales:l={l} and w={w}");
+            //    return area;
+            //}
+            //public int Calc2(ref int l, ref int w)
+            //{
+            //    l = l + 2;
+            //    w = w + 2;
+            //    int area = l * w;
+
+            //    Console.WriteLine($"vales:l={l} and w={w}");
+            //    return area;
+            //}
+            //}
+            //using out
+            //    public int Calc2(out int l, out int w)
+            //{
+            //        l = 2;
+            //        w = 2;
+            //    l = l + 2;
+            //    w = w + 2;
+            //    int area = l * w;
+
+            //    Console.WriteLine($"vales:l={l} and w={w}");
+            //    return area;
+        //    //}
+
+
+
+            //obsolete
+        //    [Obsolete("Use Calc2 instead.")]
+        //    public int Calc(int l, int w)
+        //    {
+        //        l = l + 2;
+        //        w = w + 2;
+        //        int area = l * w;
+        //        Console.WriteLine($"vales:l={l} and w={w}");
+        //        return area;
+        //    }
+        //    public int Calc2(out int l, out int w)
+        //    {
+        //        l = 2;
+        //        w = 2;
+        //        l = l + 2;
+        //        w = w + 2;
+        //        int area = l * w;
+
+        //        Console.WriteLine($"vales:l={l} and w={w}");
+        //        return area;
+        //    }
+        //}
+
+        //public class months
+        //{
+        //    public enum days
+        //    {
+        //        Monday,
+        //        Tuesday,
+        //        Wednesday,
+        //        Thursday=5,
+        //        Friday
+        //    }
+        //}
+
+
+        //indexers
+        //public class Months
+        //{
+        //    string[] result = new string[12];
+        //    public string this[int index]
+        //    {
+        //        get
+        //        {
+        //            return result[index];
+        //        }
+        //        set
+        //        {
+        //            result[index] = value;
+        //            Console.WriteLine($"{index+1} :{value}");
+        //        }
+        //    }
+        //}
+    }
+}

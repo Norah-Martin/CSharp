405df51fd16ccf7d85be1660c6be1703d86714da
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-11-04 12:49:36 +0530

    Notes on DELEGATES and Event Hnadler
    04-11-2024: delegate and event handler

---
+++ Delegate_Event/Employee.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample4
+{
+    //    public class Employee
+    //    {
+    //        public string Name {  get; set; }
+    //        public int Id {  get; set; }
+    //        public void add(Employee emp)
+    //        {
+    //            Console.WriteLine($"{emp.Name}  --{emp.Id}");
+    //        }
+    //    }
+
+
+    //way2
+
+    //public static class Employee
+    //{
+       
+    //    public static void add(dynamic emp)
+    //    {
+    //        Console.WriteLine($"{emp.Name}  --{emp.Id}");
+    //    }
+    //}
+
+    //eq
+    //public static class Fruit
+    //{
+    //    public static void Display(dynamic f)
+    //    {
+    //        Console.WriteLine($"{f.Name}---{f.color}");
+    //    }
+    //}
+
+
+    //delegate
+    //public  class Employee
+    //{
+
+    //    public void/*int*/ Add(int a, int b)
+    //    {
+            
+    //        Console.WriteLine("THIS IS ADDITION");
+    //        /*return*/
+    //    }
+
+    //    public void Sub(int a, int b)
+    //    {
+    //        Console.WriteLine("SUBTRACTION");
+    //    }
+
+    //    public void Color(string color)
+    //    {
+    //        Console.WriteLine($"color is :{color}");
+    //    }
+    //    public int division(out int a, ref int b)
+    //    {
+    //        a = 25;
+    //        b = b-5;
+    //        return a/b;
+    //    }
+
+
+
+    //}
+
+    //event
+    public class Employee
+    {
+        //public static int total;
+        //public event EventHandler TestEventHandler;
+
+        //public void addition()
+        //{
+        //    total++;
+        //    if(total>5)
+        //    {
+
+        //        //invoked here
+
+        //        TestEventHandler?.Invoke(this, EventArgs.Empty);
+        //    }
+        //}
+        public static int count;
+        public event EventHandler FruitHandler;
+        public void counter()
+        {
+            count++;
+            if(count==3)
+            {
+                FruitHandler?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+    }
+
+
+}
---
+++ Delegate_Event/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample4
+{
+    internal class Program
+    {
+
+        //DELEGATE
+        public delegate void Addition(int a, int b);
+        public delegate int Division(out int a, ref int b);
+        //public delegate int add(int a,int b);
+        //public delegate void Subtraction(int a, int b);
+
+        //public delegate void Colordisplay(string c);
+        static void Main(string[] args)
+        {
+            // //var keyword
+            // var test = "123";
+            //// test = 2;//compiler will throw error
+            // dynamic b = 1;
+            //  b = "norah";//  will not throw error
+            // Console.WriteLine(test.GetTypeCode());
+            // Console.WriteLine(b.GetTypeCode());
+
+
+            //passing value to class in function
+
+            //usual way
+            //Employee employee = new Employee();
+            //employee.Name = "norah";
+            //employee.Id = 1;
+            //employee.add(employee);
+
+            //way2---anonymous type
+            //var test = new { Name = "norah", Id = 1 };
+            //Employee.add(test);
+            //Console.ReadLine();
+
+            //eg
+
+
+            //var fruit = new { Name = "Apple", color = "Red" };
+            //Fruit.Display(fruit);
+
+            //Console.WriteLine("\n");
+            //dynamic fruit2 = new { Name = "Orange", color = "Orange" };
+            //Fruit.Display(fruit2);
+
+            //Console.WriteLine("\n");
+            //fruit2= new { Name = "Grapes", color = "violet" };
+            //Fruit.Display(fruit2);
+            ////Console.WriteLine(fruit2.Fruit.color);
+
+            //delegate
+
+            //Employee employee = new Employee();
+            ////employee.Add();
+            ////employee.Sub();
+            //Addition addition = new Addition(employee.Add);
+            //addition(50,100);
+            //Subtraction subtraction = new Subtraction(employee.Sub);
+            //subtraction(100, 50);
+
+            //Employee emp = new Employee();
+            //Colordisplay color = new Colordisplay(emp.Color);
+            //color("orange")
+            //;
+
+            //Employee EMP= new Employee();
+            // Addition A = new Addition(EMP.Add);
+            // A += EMP.Sub; //this is okay all of the datatypes should be same
+
+            // A(4, 5);
+
+            //Division d = new Division(EMP.division);
+            //Console.WriteLine(d(4, 2));
+
+            //using out
+            //Division d = new Division(EMP.division);
+            //// Declare variables for out parameters
+            //int x, y;
+            //y = 10;
+
+            //// Call the delegate and capture the return value
+            //int result = d(out x, ref y);// to get output we need to pass parameters;
+
+            //Console.WriteLine("Division result: " + result);
+
+            //event
+            Employee employee = new Employee();
+            //    employee.TestEventHandler += Employee_TestEventHandler;
+            //    Console.WriteLine("enter values: ");
+            //    int value = 0;
+            //    while (value < 6)
+            //    {
+            //        var result = Console.ReadKey(true).KeyChar;
+            //        // Console.WriteLine(result);
+            //        value++;
+            //        Console.WriteLine(value);
+
+            //    }
+
+            //    Console.ReadLine();
+
+
+            //public static void Employee_TestEventHandler(object SENDER, EventArgs E)
+            //{
+            //    Console.WriteLine("THRESHOLD");
+            //}
+
+           
+            employee.FruitHandler += Employee_FruitEventHandler;
+            while (true)
+            {
+                Console.WriteLine("enter the colors: ");
+                string value = Console.ReadLine();
+                employee.counter();
+                if (value == "red")
+                {
+                    Console.WriteLine("------------");
+                    Console.WriteLine(value);
+                  
+                    break;
+                }
+            }
+            
+            Console.ReadLine();
+
+        } 
+        private static void Employee_FruitEventHandler(object sender,EventArgs e)
+        {
+            Console.WriteLine("maximum value written");
+        }
+
+
+    }
+}

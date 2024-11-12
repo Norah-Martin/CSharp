98f79278d02223af19f48ecd53a307ede9096739
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-11-12 11:16:33 +0530

    Unit Tests
    Unit tests-give testcases

---
+++ UnitTest/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Employee emp = new Employee();
+            var result=emp.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Employee emp = new Employee();
+            var result = emp.Add(1, 3);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Employee emp = new Employee();
+            var result = emp.Add(3, 3);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
